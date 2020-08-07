de17b58e7300ce03b0badff16a2aa7f73ed71576
Jonathan Perry <Jonathan.Perry1994@gmail.com>
2020-08-07 18:50:18 -0500

    testing node-schedule

--- Soil Sensor API/node-schedule-test.js
+++ Soil Sensor API/node-schedule-test.js
@@ -1,14 +1,14 @@
 var schedule = require('node-schedule');
 
-var date = new Date(2020, 7, 7, 18, 50, 0);
+var date = new Date(2020, 7, 7, 18, 51, 0);
 
 var j = schedule.scheduleJob(date, function(){
-    // var x = schedule.scheduleJob('*/1 * * * *', function(){
+    var x = schedule.scheduleJob('*/1 * * * *', function(){
     
         
-    //   console.log(`This should execute every minute starting at: `, date);
-    // });
-    // console.log(`x: ${x.nextInvocation()}`);
+      console.log(`This should execute every minute starting at: `, date);
+    });
+    console.log(`x: ${x.nextInvocation()}`);
 
 });
 console.log(`j: ${j.nextInvocation()}`);
