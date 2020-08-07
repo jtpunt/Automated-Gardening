baea60be8e7ca6eaf8cb3494591fa6a65b68cd9a
Jonathan Perry <Jonathan.Perry1994@gmail.com>
2020-08-07 18:33:10 -0500

    testing node-schedule

--- Soil Sensor API/node-schedule-test.js
+++ Soil Sensor API/node-schedule-test.js
@@ -4,3 +4,9 @@ var j = schedule.scheduleJob('* */5 * * *', function(){
   console.log('This should execute every 5 hours');
 });
 console.log(j);
+
+console.log(`Every 5 Hours: ${j.nextInvocation()}`);
+
+j.reschedule('*/5 * * * *');
+
+console.log(`Every 5 minutes: ${j.nextInvocation()}`);
