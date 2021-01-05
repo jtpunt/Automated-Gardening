c7a7d852c1592c3e5620538b730f319ba4e30f33
jtpunt <jonathan.perry1994@gmail.com>
2021-01-04 21:59:45 -0600

    updated JS to show which step the user is on with the new schedule form

--- Temperature and Humidity Controller/public/js/schedule/common.js
+++ Temperature and Humidity Controller/public/js/schedule/common.js
@@ -25,3 +25,100 @@ function selectCheckbox(id){
 function collapseMenu(id){
 	$('#' + id).collapse('hide');
 }
+// tableId = vegWaterTable or floWaterTable
+function waterSelectChanged(e, tableId){
+    let tableEle = document.getElementById(tableId),
+        numOfRows = e.value,
+        i = 0,
+        curRows = tableEle.children.length - 1; // minus the table headers
+
+    console.log(`numOfRows: ${numOfRows}`);
+    console.log(`curRows: ${curRows}`);
+    if(curRows >= 0 && numOfRows > curRows){
+        console.log(curRows);
+        i = curRows;
+        for(; i < numOfRows; i++){
+            let divRow   = document.createElement("div"), // <div class="rTableRow">
+                divCellWeek = document.createElement("div"), // <div class="rTableCell">Week 1</div>
+                divCellDates = document.createElement("div"), // <div class="rTableCell">Dates</div>
+                divCellPumpTime = document.createElement("div"), // <div class="rTableCell">Pump Run Time</div>
+                divCellInchesOfWater = document.createElement("div"), // <div class="rTableCell">Inches of Water</div>
+                divCellCupsOfWater = document.createElement("div"); //<div class="rTableCell">Cups of Water</div>
+        
+            divRow.className = "rTableRow";
+            divCellWeek.className = "rTableCell";
+            divCellDates.className = "rTableCell";
+            divCellPumpTime.className = "rTableCell";
+            divCellInchesOfWater.className = "rTableCell";
+            divCellCupsOfWater.className = "rTableCell";
+
+            divCellWeek.innerText = "Week " + (i + 1);
+
+            tableEle.appendChild(divRow);
+            divRow.appendChild(divCellWeek);
+            divRow.appendChild(divCellDates);
+            divRow.appendChild(divCellPumpTime);
+            divRow.appendChild(divCellInchesOfWater);
+            divRow.appendChild(divCellCupsOfWater);
+        }
+    }else{
+        let rowsToRemove = curRows - numOfRows;
+
+        console.log(`We need to remove ${rowsToRemove} rows`);
+        console.log(tableEle.children);
+        for(let j = curRows; j > curRows - rowsToRemove; j--){
+            console.log("Removing row?");
+            console.log(`j = ${j}`);
+            console.log(`curRows = ${curRows}`);
+            tableEle.removeChild(tableEle.children[j]);
+        }
+    }
+
+
+    console.log(tableEle);
+}
+function nextPrev(e, operand) {
+    console.log(e, operand);
+	let ids = [
+		"scheduleFieldset", 
+		"vegWaterFieldset",
+		"floWaterFieldset", 
+		"reviewFieldset", 
+		"confirmFieldset"
+	], 
+        activeSectionId = "",
+        activeSectionIdx = -1,
+        activeSectionEle = "";
+    ids = ids.filter(id => document.getElementById(id) !== null);
+
+	ids.forEach(function(id, i){
+		let idEle = document.getElementById(id);
+        if(idEle.hidden === false){
+            idEle.hidden = true;
+            activeSectionId = id;
+            activeSectionIdx = i;
+        }
+	})
+    // Are we at the last fieldset? Remove the next button
+    if(activeSectionIdx + operand === ids.length - 1){
+        e.style.display = "none"; 
+    }
+    // Are we not on the last fieldset? Show the next button
+    if(activeSectionIdx + operand < ids.length - 1){
+        document.getElementById("nextBtn").style.display = "inline";
+    }
+    // Are we on the first field set? Remove the previous button
+    if(activeSectionIdx + operand === 0){
+        e.style.display = "none";
+    }
+    // Are we after the first fieldset? Show the previous button
+    if(activeSectionIdx + operand > 0){
+        document.getElementById("prevBtn").style.display = "inline";
+    }
+    activeSectionEle = document.getElementById(ids[activeSectionIdx + operand]);
+    activeSectionEle.hidden = false;
+
+    // Help show progress of where the user is on the form
+    document.getElementById("step" + activeSectionIdx).classList.remove("active");
+    document.getElementById("step" + (activeSectionIdx + operand)).className += " active";
+}
