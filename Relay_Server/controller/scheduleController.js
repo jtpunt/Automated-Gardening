cb18fb20349d934670ee7674c03a0e47c50f5e16
jtpunt <jonathan.perry1994@gmail.com>
2021-01-11 12:29:59 -0600

    debugging

--- Relay_Server/controller/scheduleController.js
+++ Relay_Server/controller/scheduleController.js
@@ -1,6 +1,6 @@
 var Scheduler       = require("../models/scheduler"),
     Device          = require("../models/device"),
-    scheduleHelpers = require("../controller/helpers/scheduleHelpers"),
+    scheduleHelpers = require("../helpers/scheduleHelpers"),
     schedule        = require('node-schedule'),
     ip              = require("ip"),
     async           = require("asyncawait/async"),
