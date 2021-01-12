b801c5a3d4238bc7f1eb3fcdddd577bcdd8b4a08
jtpunt <jonathan.perry1994@gmail.com>
2021-01-12 15:51:24 -0600

    Update scheduleMiddleware.js

--- Relay_Server/middleware/scheduleMiddleware.js
+++ Relay_Server/middleware/scheduleMiddleware.js
@@ -16,7 +16,8 @@ let scheduleMiddleware = {
         }
     },
     checkScheduleInputs(req, res, next){
-        var newSchedule = req.body;
+        var newSchedule = req.body,
+            sanitizedSchedule = {};
         try{
         // newSchedule = {
         //     schedule: {
@@ -64,6 +65,11 @@ let scheduleMiddleware = {
                         throw new Error("End Time Minute configuration details not found.")
                     if(newSchedule['schedule']['end_time']['hour'] === undefined)
                         throw new Error("End Time hour configuration details not found.")
+                    sanitizedSchedule['schedule']['end_time'] = {
+                        second: newSchedule['schedule']['end_time']['second'],
+                        minute: newSchedule['schedule']['end_time']['minute'],
+                        hour: newSchedule['schedule']['end_time']['hour']
+                    }
                 }
                 // start_time (on) details are not required
                 if(newSchedule['schedule']['start_time'] !== undefined){
@@ -73,6 +79,11 @@ let scheduleMiddleware = {
                         throw new Error("End Time Minute configuration details not found.")
                     if(newSchedule['schedule']['start_time']['hour'] === undefined)
                         throw new Error("End Time hour configuration details not found.")
+                    sanitizedSchedule['schedule']['start_time'] = {
+                        second: newSchedule['schedule']['start_time']['second'],
+                        minute: newSchedule['schedule']['start_time']['minute'],
+                        hour: newSchedule['schedule']['start_time']['hour']
+                    }
                 }
                 // Check For Date Based Scheduling Details
                 if(newSchedule['schedule']['start_date'] !== undefined){
@@ -83,6 +94,11 @@ let scheduleMiddleware = {
                         throw new Error("Month input required for date-based scheduling");
                     if(newSchedule['schedule']['start_date']['year'] === undefined)
                         throw new Error("Year input requried for date-based scheduling")
+                    sanitizedSchedule['schedule']['start_date'] = {
+                        date: newSchedule['schedule']['start_date']['date'],
+                        month: newSchedule['schedule']['start_date']['month'],
+                        year: newSchedule['schedule']['start_date']['year']
+                    }
                 }
                 // Check For Date Based Scheduling Details
                 if(newSchedule['schedule']['end_date'] !== undefined){
@@ -93,6 +109,11 @@ let scheduleMiddleware = {
                         throw new Error("Month input required for date-based scheduling");
                     if(newSchedule['schedule']['end_date']['year'] === undefined)
                         throw new Error("Year input requried for date-based scheduling")
+                    sanitizedSchedule['schedule']['end_date'] = {
+                        date: newSchedule['schedule']['end_date']['date'],
+                        month: newSchedule['schedule']['end_date']['month'],
+                        year: newSchedule['schedule']['end_date']['year']
+                    }
                 }
                 // Check For Recurrence Based Scheduling details
                 if(newSchedule['schedule']['dayOfWeek'] !== undefined){
@@ -103,6 +124,7 @@ let scheduleMiddleware = {
                         throw new Error("Recurrence Based Scheduling is not valid with date-based scheduling details");
                     if(newSchedule['schedule']['start_date']['year'] !== undefined)
                         throw new Error("Recurrence Based Scheduling is not valid with date-based scheduling details");
+                    sanitizedSchedule['schedule']['dayOfWeek'] = newSchedule['schedule']['dayOfWeek'];
                 }
             }
             // device details are required
@@ -112,13 +134,9 @@ let scheduleMiddleware = {
                 // id - mongodb id representing our relay device - required
                 if(newSchedule['device']['id'] === undefined)
                     throw new Error("Device id not found!");
-                else{
-                    // Make sure that the id is a valid id that exists in mongodb
-                }
                 // gpio port that controls our relay switch - required
                 if(newSchedule['device']['gpio'] === undefined)
                     throw new Error("Device GPIO not found!");
-
                 // 0 or 1, on or off? - required
                 if(newSchedule['device']['desired_state'] === undefined)
                     throw new Error("Device desired state not found!");
@@ -127,8 +145,14 @@ let scheduleMiddleware = {
                     if(typeof newSchedule['device']['desired_state'] === 'boolean')
                         throw new Error("Desired state must be 'true' or 'false'.")
                 }
+                sanitizedSchedule['device'] = {
+                    id: newSchedule['device']['id'],
+                    gpio: newSchedule['device']['gpio'],
+                    desired_state: newSchedule['device']['desired_state']
+                }
             }
         }
+            req.body = sanitizedSchedule;
             next();
         }catch(exc){
                     console.log(`err: ${exc}`);
