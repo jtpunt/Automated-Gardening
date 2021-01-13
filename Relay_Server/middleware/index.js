011a84d555f75b932be32d4dc3a3f4b802b06d82
jtpunt <jonathan.perry1994@gmail.com>
2021-01-12 21:22:21 -0600

    Update index.js

--- Relay_Server/middleware/index.js
+++ Relay_Server/middleware/index.js
@@ -3,13 +3,16 @@ let User = require("../models/user");
 var middleware = {
 	verifyAdminAccount(req, res, next){
 	    let scheduleObj = req.body,
-	        admin_id    = scheduleObj['admin_id'].toString();
-	        
-	    console.log("verifyAdminAccount: " + admin_id);
+	        admin_id    = scheduleObj['admin_id'] || undefined,
+	        notAdminMsg = "you must be an admin to perform that action";
+
+	    if(admin_id === undefined)
+	    	res.status(404).send(notLoggedinMsg);
+
 		User.findOne({"_id":admin_id}, function(err, user){
 		    if(err){
 		        console.log(err.toString);
-		        res.status(404).send("you must be logged in as an admin to perform that action");
+		        res.status(404).send(notLoggedinMsg);
 		    }
 		    else{
 		        console.log("user: " + user);
