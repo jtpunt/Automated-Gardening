5d7bd05227a947250a9cd40e47e6ca4c9ca61808
jtpunt <jonathan.perry1994@gmail.com>
2021-01-12 19:47:43 -0600

    Update scheduleController.js

--- Relay_Server/controller/scheduleController.js
+++ Relay_Server/controller/scheduleController.js
@@ -860,14 +860,12 @@ var scheduleObj = {
         console.log("in delete schedule");
         return function(req, res, next){
             var schedule_id = req.params.schedule_id;
-            try{
+
                 let index = self.findScheduleIndex("123");
                 console.log(`index: ${index}`);
                 // self.deleteSchedule("123");
                 res.status(200).end();
-            }catch(err){
-                res.status(404).send(err.toString());
-            }
+
         }
     },
     deleteSchedules: function(...schedule_ids){
