a7037c640d7d56f1264a96d934304ae2f289c1ad
jtpunt <jonathan.perry1994@gmail.com>
2021-01-12 21:31:36 -0600

    Update scheduleController.js

--- Relay_Server/controller/scheduleController.js
+++ Relay_Server/controller/scheduleController.js
@@ -875,7 +875,7 @@ var scheduleObj = {
     findScheduleIndex: function(schedule_id){
         let index = this.scheduleArr.findIndex((scheduleObj) => scheduleObj['schedule_config']['_id'] == schedule_id);
         if(index === -1)
-            throw new Error(`Schedule id: ${schedule_id} not found!`);
+            throw (`Schedule id: ${schedule_id} not found!`);
         return index;
     }
 }
