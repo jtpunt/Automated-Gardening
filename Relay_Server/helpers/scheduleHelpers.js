c1575f56d318045ce0533d20a81e8c094eff8bd3
jtpunt <jonathan.perry1994@gmail.com>
2021-01-15 23:12:14 -0600

    reverting

--- Relay_Server/helpers/scheduleHelpers.js
+++ Relay_Server/helpers/scheduleHelpers.js
@@ -355,9 +355,9 @@ let scheduleHelpers = {
     },
     isScheduleOverlapping: function(on_schedule_config, off_schedule_config){
         let self              = this,
-            new_on_second     = Number(on_schedule_config['schedule']['second']) || undefined,
-            new_on_minute     = Number(on_schedule_config['schedule']['minute']) || undefined,
-            new_on_hour       = Number(on_schedule_config['schedule']['hour'  ]) || undefined,
+            new_on_second     = on_schedule_config['schedule']['second'] || undefined,
+            new_on_minute     = on_schedule_config['schedule']['minute'] || undefined,
+            new_on_hour       = on_schedule_config['schedule']['hour'  ] || undefined,
             
             new_off_second    = off_schedule_config['schedule']['second'] || undefined,
             new_off_minute    = off_schedule_config['schedule']['minute'] || undefined,
