b458a7e8ddc980ad892ac1c2712dcd937fdd81ff
jtpunt <jonathan.perry1994@gmail.com>
2021-01-15 23:49:36 -0600

    Update scheduleMiddleware.js

--- Relay_Server/middleware/scheduleMiddleware.js
+++ Relay_Server/middleware/scheduleMiddleware.js
@@ -175,7 +175,7 @@ let scheduleMiddleware = {
         console.log(`in validateScheduleInputs with ${JSON.stringify(schedule)}`);
 
 
-        let validSchedule = function(schedule){
+        let buildSchedule = function(schedule){
             if(schedule === undefined) return schedule;
             let validSchedule = {}
             const 
