f9da8ca27bd743bb408e962e3fe635266a817460
jtpunt <jonathan.perry1994@gmail.com>
2021-01-23 15:56:26 -0600

    added automatic yes response for install bash script

--- Relay_Server/install.sh
+++ Relay_Server/install.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 sudo apt-get autoclean
 sudo apt-get update
-sudo apt-get upgrade
+yes Y | sudo apt-get upgrade # pipe in 'Y' to install packages
 sudo apt-get dist-upgrade
 wget -O - https://raw.githubusercontent.com/audstanley/NodeJs-Raspberry-Pi/master/Install-Node.sh | sudo bash;
-sudo node-install -v 15;
+# enter 9 for selecting version 15.6
+9 | sudo node-install -v 15;
 npm install
 crontab -l > oldcrontab
 cp oldcrontab newcrontab
 echo "@reboot /home/pi/Smart-Gardening/Relay_Server/start.sh > /tmp/listener1.log 2>&1" >> newcrontab
 crontab < newcrontab
+sudo timedatectl set-timezone America/Chicaco
+sudo nano /etc/dhcpcd.conf
