fa4c5950c7367133679451a29d105893760c1182
jtpunt <jonathan.perry1994@gmail.com>
2021-01-23 19:28:34 -0600

    Update install.sh

--- Relay_Server/install.sh
+++ Relay_Server/install.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 yes Y | sudo apt-get upgrade # pipe in 'Y' to install packages
 sudo apt-get dist-upgrade
 wget -O - https://raw.githubusercontent.com/audstanley/NodeJs-Raspberry-Pi/master/Install-Node.sh | sudo bash;
-9 | sudo node-install -v 15; # enter 9 for selecting version 15.6
+sudo node-install -v 15; # enter 9 for selecting version 15.6
 npm install
 crontab -l > oldcrontab
 cp oldcrontab newcrontab
