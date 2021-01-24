8b0a6b5cde7b5ed7f620e9c316da7d0ecf9f4bd7
jtpunt <jonathan.perry1994@gmail.com>
2021-01-23 19:16:51 -0600

    Update install.sh

--- Relay_Server/install.sh
+++ Relay_Server/install.sh
@@ -4,12 +4,10 @@ sudo apt-get update
 yes Y | sudo apt-get upgrade # pipe in 'Y' to install packages
 sudo apt-get dist-upgrade
 wget -O - https://raw.githubusercontent.com/audstanley/NodeJs-Raspberry-Pi/master/Install-Node.sh | sudo bash;
-# enter 9 for selecting version 15.6
-9 | sudo node-install -v 15;
+9 | sudo node-install -v 15; # enter 9 for selecting version 15.6
 npm install
 crontab -l > oldcrontab
 cp oldcrontab newcrontab
 echo "@reboot /home/pi/Smart-Gardening/Relay_Server/start.sh > /tmp/listener1.log 2>&1" >> newcrontab
 crontab < newcrontab
 sudo timedatectl set-timezone America/Chicaco
-sudo nano /etc/dhcpcd.conf
