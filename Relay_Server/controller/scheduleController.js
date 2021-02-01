3379560be17f6242bf333c2924289571191eca54
jtpunt <jonathan.perry1994@gmail.com>
2021-01-31 20:23:45 -0600

    Update scheduleController.js

--- Relay_Server/controller/scheduleController.js
+++ Relay_Server/controller/scheduleController.js
@@ -92,7 +92,7 @@ var scheduleMethods = {
                 },
                 on_end_schedule_time = {
                     ... newSchedule['schedule']['end_date'],
-                    ... newSchedule['schedule']['end_time']  
+                    ... newSchedule['schedule']['start_time']  
                 },
                 off_end_schedule_time = {
                     ... newSchedule['schedule']['end_date'],
