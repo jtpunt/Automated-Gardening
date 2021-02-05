300d8ba5f88bac2ace387840ab1f8fb637b829e5
jtpunt <jonathan.perry1994@gmail.com>
2021-02-04 19:42:28 -0600

    Update outlet.js

--- Relay_Server/classes/outlet.js
+++ Relay_Server/classes/outlet.js
@@ -28,7 +28,7 @@ class Outlet{
 	// #direction
 	constuctor(relay_settings){
 		// super(relay_settings);
-		this.options = { reconfigureDirection: true };
+		this.relay_settings = relay_settings;
 		console.log(`in outlet const with: ${JSON.stringify(relay_settings)}`);
 		// this.#outlet = new Gpio(this.gpio, this.direction, this.options);
 		// // if readSync() is 1 after initializing the GPIO, this does not mean it's on, it's really off
