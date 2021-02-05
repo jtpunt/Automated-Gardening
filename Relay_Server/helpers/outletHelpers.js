e0764f65cd940d6dfa59d489f0950efa8063fe80
jtpunt <jonathan.perry1994@gmail.com>
2021-02-04 21:02:15 -0600

    Update outletHelpers.js

--- Relay_Server/helpers/outletHelpers.js
+++ Relay_Server/helpers/outletHelpers.js
@@ -55,10 +55,10 @@ var outletHelpers = {
                 .withRelaySettings(relay_config)
                 .build();
             self.outletObj[relay_config['relayId']] = outlet;
-            console.log(`outlet: ${JSON.stringify(outlet)}`);
-            console.log(`status: ${outlet.status}`);
-            outlet.activate = 1;
-            console.log(`status: ${outlet.status}`);
+            // console.log(`outlet: ${JSON.stringify(outlet)}`);
+            // console.log(`status: ${outlet.status}`);
+            // outlet.activate = 1;
+            // console.log(`status: ${outlet.status}`);
         //     Device.findOne({
         //         local_ip: localIP, 
         //         deviceType: "Relay Server"
@@ -183,46 +183,59 @@ var outletHelpers = {
         //         }
         //     }
         // },
-        // activateRelay: function(gpio_input, desired_state) {
-        //     let self = this;
-        //     let index = self.findOutletByGpio(gpio_input);
-        //     console.log("in activateRelay\n");
-        //     if(index !== -1){
-        //         let status = self.getStatus(gpio_input);
-        //         console.log(`Status: ${status}, desired_state: ${desired_state}`);
-        //         // typeof status === "Number"
-        //         // typeof desired_state === "Boolean"
-        //         if(status === Number(desired_state)){
-        //             console.log("Device is already in the desired state!");
-        //             return;
-        //         }else{
-        //             console.log("Desired State: " + desired_state);
-        //             if(self.outletArr[index]['initialState'] === 1){ // seems like 1 is equal to on, but it is opposite and means 1 is off
-        //                 console.log("desired state is opposite due to initialState");
-        //                 desired_state ^= 1;
-        //             }
-        //             self.outletArr[index]['outlet'].writeSync(Number(desired_state));  
-        //             console.log("Outlet " + gpio_input + " activated on " + new Date().toISOString() + " to " + desired_state + "\n");      
-        //         }
+        activateRelay: function(gpio_input, desired_state) {
+            let self     = this,
+                outletId = outletHelpers.getOutletIdByGpio(gpio_input);
+            if(outletId){
+                if(self.doesOutletExist(outletId)){
+                    let outlet = outletObj[outletId];
+                    outlet.activate = desired_state;
+                }else{
+
+                }
+            }
+            console.log("in activateRelay\n");
+            // if(index !== -1){
+            //     let status = self.getStatus(gpio_input);
+            //     console.log(`Status: ${status}, desired_state: ${desired_state}`);
+            //     // typeof status === "Number"
+            //     // typeof desired_state === "Boolean"
+            //     if(status === Number(desired_state)){
+            //         console.log("Device is already in the desired state!");
+            //         return;
+            //     }else{
+            //         console.log("Desired State: " + desired_state);
+            //         if(self.outletArr[index]['initialState'] === 1){ // seems like 1 is equal to on, but it is opposite and means 1 is off
+            //             console.log("desired state is opposite due to initialState");
+            //             desired_state ^= 1;
+            //         }
+            //         self.outletArr[index]['outlet'].writeSync(Number(desired_state));  
+            //         console.log("Outlet " + gpio_input + " activated on " + new Date().toISOString() + " to " + desired_state + "\n");      
+            //     }
 
                 
-        //     }else{
-        //         console.log(`index: ${index}, gpio_input: ${gpio_input}`);
-        //     }
-        // },
-        // getStatus: function(gpio_input){
-        //     let self = this;
-        //     let index = self.findOutletByGpio(gpio_input);
-        //     console.log("In getStatus\n");
-        //     if(index !== -1){
-        //         console.log("Outlet Found!\n");
-        //         let curState = self.outletArr[index]['outlet'].readSync();
-        //         if(self.outletArr[index]['initialState'] === 1){ // seems like 1 is equal to on, but it is opposite and means 1 is off
-        //             curState ^= 1;
-        //         }
-        //         return curState;
-        //     }
-        // },
+            // }else{
+            //     console.log(`index: ${index}, gpio_input: ${gpio_input}`);
+            // }
+        },
+        getStatus: function(gpio_input){
+            let self = this,
+                outletId = outletHelpers.getOutletIdByGpio(gpio_input);
+            if(outletId){
+                if(self.doesOutletExist(outletId)){
+                    return self.outletObj[outletId].status;
+                }
+            }
+            // console.log("In getStatus\n");
+            // if(index !== -1){
+            //     console.log("Outlet Found!\n");
+            //     let curState = self.outletArr[index]['outlet'].readSync();
+            //     if(self.outletArr[index]['initialState'] === 1){ // seems like 1 is equal to on, but it is opposite and means 1 is off
+            //         curState ^= 1;
+            //     }
+            //     return curState;
+            // }
+        },
         // findOutletByGpio: function(gpio_input){
         //     return this.outletArr.findIndex((outlet) => outlet['gpio'] === gpio_input);
         // },
@@ -233,6 +246,9 @@ var outletHelpers = {
 module.exports = outletHelpers;
 
 outletHelpers.getOutletSetup();
+let status = outletHelpers.getStatus(3);
+outletHelpers.activateRelay(3, 1);
+let status = outletHelpers.getStatus(3);
 let outletId = outletHelpers.getOutletIdByGpio(3);
 console.log(`outletId found? - ${outletId}`);
 
