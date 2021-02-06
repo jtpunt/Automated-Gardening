e72b0b7e4b4e6704014f37787bd084bc9623b63d
jtpunt <jonathan.perry1994@gmail.com>
2021-02-05 21:14:32 -0600

    Update outletMiddleware.js

--- Relay_Server/middleware/outletMiddleware.js
+++ Relay_Server/middleware/outletMiddleware.js
@@ -3,6 +3,7 @@ var outletMiddleware = {
         return function(req, res, next){
             let newSchedule = req.body,
             	requestedGpio = Number(newSchedule['device']['gpio']) || Number(req.params.gpio);
+            console.log(`in outletMiddleware with: ${req.params}`);
             if(outletHelper.getOutletIdByGpio(requestedGpio) === undefined){
                 res.status(404).send("Invalid GPIO input");
             }else{
