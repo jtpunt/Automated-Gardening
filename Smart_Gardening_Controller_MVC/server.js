a43a80d2297482f034c9277dec7e12b07d316a3e
jtpunt <jonathan.perry1994@gmail.com>
2021-04-09 21:32:34 -0500

    Update server.js

--- Smart_Gardening_Controller_MVC/server.js
+++ Smart_Gardening_Controller_MVC/server.js
@@ -52,7 +52,7 @@ mongoose.connect(connStr, options, function(err){
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("/public"));
 //app.use('/static', express.static('public')); // static directory is going to be our directory called public
 
 app.use(methodOverride("_method")); // _method is what we are telling it to look for
